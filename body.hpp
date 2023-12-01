f47230106040df896f8b5d14d3246018cd80af08
yuxuanye1 <113309543+yuxuanye1@users.noreply.github.com>
2023-12-01 13:53:46 -0500

    body.hpp

--- body.hpp
+++ body.hpp
@@ -1,125 +1,156 @@
-#ifndef RK4_HPP
-#define RK4_HPP
-#include "body.hpp"
+
+#ifndef BODY_HPP
+#define BODY_HPP
+
 #include <cmath>
-#include <vector>
 
 #define G 1.0
 
-// Fourth-order Runge-Kutta method
-// Let ODE be x' = f(x, t), x(0) = x0
-// Then x(t_n+1) = x(t_n) + (1/6)(k1 + 2k2 + 2k3 + k4)*h
-// where h = t_n+1 - t_n
-// k1 = f(x_n, t_n)
-// k2 = f(x_n + (1/2)k1*h, t_n + (1/2)*h)
-// k3 = f(x_n + (1/2)k2*h, t_n + (1/2)*h)
-// k4 = f(x_n + k3*h, t_n + h)
+/**
+ * @class Body
+ * @brief A class that represents a physical body in a simulation.
+ *
+ * This class contains properties such as position, velocity, acceleration, mass, and radius,
+ * and methods to calculate gravity force, check intersection with another body, absorb another body,
+ * and calculate elastic collision with another body. Mass and Radius are enforced to be positive.
+ */
+class Body {
+private:
+    double x;       // x center coordinate
+    double y;       // y center coordinate
+    double x_vel;   // velocity in the x direction
+    double y_vel;   // velocity in the y direction
+    double x_acc;   // acceleration in the x direction
+    double y_acc;   // acceleration in the y direction
+    double mass;    // mass of the body
+    double radius;   // radius of the body
 
-// Let x be a vector of size 2*n*dim, where n is the number of bodies and dim is the dimension of the system
-// x = [pos1, pos2, ..., posn, vel1, vel2, ..., veln]
-// Let f(x, t) = [vel1, vel2, ..., veln, acc1, acc2, ..., accn]
-// Then x' = f(x, t) = [vel1, vel2, ..., veln, acc1, acc2, ..., accn]
+public:
+    //Default constructor
+    Body()
+        : x(0), y(0), x_vel(0), y_vel(0), x_acc(0), y_acc(0), mass(1), radius(1) {}
 
-// store trajectory of a body during a time step by vector<vector<double>>
 
-// write a function for f(x, t)
-// return f with a vector of size 2*n*dim same as x
-std::vector<double> one_iter(std::vector<double> &x, size_t n, size_t dim, std::vector<double> &masses){
-    // x = [pos1, pos2, ..., posn, vel1, vel2, ..., veln]
-    // f(x, t) = [vel1, vel2, ..., veln, acc1, acc2, ..., accn]
-    // initialize f with zeros
-    std::vector<double> f(2*n*dim, 0);
-    // first n*dim elements are velocities
-    for (size_t i = 0; i < n*dim; i++){
-        f[i] = x[i + n*dim];
-    }
-    // last n*dim elements are accelerations
-    // compute accelerations between each pair of bodies
-    for (size_t i = 0; i < n; i++){
-        for (size_t j = i+1; j < n; j++){
-            // compute distance between body i and body j
-            double dist = 0;
-            for (size_t k = 0; k < dim; k++){
-                dist += pow(x[i*dim + k] - x[j*dim + k], 2);
+    //Constructor
+    Body(double x, double y, double x_vel, double y_vel, double x_acc, double y_acc, double mass, double radius)
+        : x(x), y(y), x_vel(x_vel), y_vel(y_vel), x_acc(x_acc), y_acc(y_acc), mass(mass), radius(radius) {
+            if (mass <= 0) {
+                std::cout << "Warning: Input mass is non-positive. Setting mass to 1." << std::endl;
+                this->mass = 1;
             }
-            dist = sqrt(dist);
-            // compute acceleration of body i and body j
-            double acc = G/(dist*dist*dist);
-            // update acceleration of body i 
-            for (size_t k = 0; k < dim; k++){
-                f[i*dim + n*dim + k] += acc*masses[j]*x[j*dim + k];
-            }
-            // update acceleration of body j
-            for (size_t k = 0; k < dim; k++){
-                f[j*dim + n*dim + k] -= acc*masses[i]*x[i*dim + k];
+            if (radius <= 0) {
+                std::cout << "Warning: Input radius is non-positive. Setting radius to 1." << std::endl;
+                this->radius = 1;
             }
         }
-    }
-    return f;
-}
 
-
-std::vector<std::vector<double>> RK4(std::vector<Body> &bodies, double timeStep, double endTime){
-    // number of bodies
-    size_t n = bodies.size();
-    // dimension of the system
-    size_t dim = 2;
-    // number of time steps
-    size_t numSteps = endTime/timeStep;
-    // store trajectory of each body with n*dim columns and numSteps+1 rows
-    std::vector<std::vector<double>> trajectory(numSteps+1, std::vector<double>(n*dim, 0));
-    // initialize trajectory only contains positions of bodies
-    for (size_t i = 0; i < n; i++){
-        trajectory[0][i*dim] = bodies[i].getX();
-        trajectory[0][i*dim + 1] = bodies[i].getY();
-    }
-    // initialize masses
-    std::vector<double> masses(n, 0);
-    for (size_t i = 0; i < n; i++){
-        masses[i] = bodies[i].getMass();
-    }
-    // iterate over time steps
-    // use one_iter to compute f(x, t)
-    // update x(t_n+1) = x(t_n) + (1/6)(k1 + 2k2 + 2k3 + k4)*h
-    for (size_t i = 0; i < numSteps; i++){
-        // compute k1
-        std::vector<double> k1 = one_iter(trajectory[i], n, dim, masses);
-        // compute k2
-        std::vector<double> k2(2*n*dim, 0);
-        for (size_t j = 0; j < 2*n*dim; j++){
-            k2[j] = trajectory[i][j] + 0.5*timeStep*k1[j];
-        }
-        k2 = one_iter(k2, n, dim, masses);
-        // compute k3
-        std::vector<double> k3(2*n*dim, 0);
-        for (size_t j = 0; j < 2*n*dim; j++){
-            k3[j] = trajectory[i][j] + 0.5*timeStep*k2[j];
-        }
-        k3 = one_iter(k3, n, dim, masses);
-        // compute k4
-        std::vector<double> k4(2*n*dim, 0);
-        for (size_t j = 0; j < 2*n*dim; j++){
-            k4[j] = trajectory[i][j] + timeStep*k3[j];
-        }
-        k4 = one_iter(k4, n, dim, masses);
-        // update trajectory
-        for (size_t j = 0; j < 2*n*dim; j++){
-            trajectory[i+1][j] = trajectory[i][j] + (1.0/6.0)*(k1[j] + 2*k2[j] + 2*k3[j] + k4[j])*timeStep;
-        }
-    }
-    // update bodies
-    // set X and X_vel of each body
-    // also set Y and Y_vel of each body
-    for (size_t i = 0; i < n; i++){
-        bodies[i].setX(trajectory[numSteps][i*dim]);
-        bodies[i].setX_vel(trajectory[numSteps][i*dim + n*dim]);
-        bodies[i].setY(trajectory[numSteps][i*dim + 1]);
-        bodies[i].setY_vel(trajectory[numSteps][i*dim + n*dim + 1]);
+    //Getters
+    double getX() const {
+        return x;
     }
 
-    return trajectory;
-}
+    double getY() const {
+        return y;
+    }
+
+    double getX_vel() const {
+        return x_vel;
+    }
+
+    double getY_vel() const {
+        return y_vel;
+    }
+
+    double getX_acc() const {
+        return x_acc;
+    }
+
+    double getY_acc() const {
+        return y_acc;
+    }
+
+    double getMass() const {
+        return mass;
+    }
+
+    double getRadius() const {
+        return radius;
+    }
+
+    //Setters
+    void setX(double newX) {
+        x = newX;
+    }
+
+    void setY(double newY) {
+        y = newY;
+    }
+
+    void setX_vel(double newVelocityX) {
+        x_vel = newVelocityX;
+    }
+
+    void setY_vel(double newVelocityY) {
+        y_vel = newVelocityY;
+    }
+
+    void setX_acc(double newAccelerationX) {
+        x_acc = newAccelerationX;
+    }
+
+    void setY_acc(double newAccelerationY) {
+        y_acc = newAccelerationY;
+    }
+
+    void setMass(double newMass) {
+        mass = newMass;
+    }
+
+    void setRadius(double newRadius) {
+        radius = newRadius;
+    }
+
+    //Calculate the force of gravity exerted from another body
+    double* calculateGravityForce(const Body& otherBody) const {
+        //calculate the magnitude of the force between the two bodies
+        double distance = std::sqrt(std::pow(otherBody.getX() - x, 2) + std::pow(otherBody.getY() - y, 2));
+        double force = -1.0*(G * mass * otherBody.getMass()) / std::pow(distance, 2);
+
+        //calculate the direction of the force between the two bodies
+        //calculate the displacement vector from this body to the other body
+        double* displacementVector = new double[2];
+        displacementVector[0] = x - otherBody.getX();
+        displacementVector[1] = y - otherBody.getY();
+        //normalize the displacement vector
+        double* forceVector = new double[2];
+        forceVector[0] = force * displacementVector[0] / distance;
+        forceVector[1] = force * displacementVector[1] / distance;
+
+        // free up the memory allocated for the displacement vector
+        delete[] displacementVector;
+        return forceVector;
+    }
+
+    //Check if this body is currently intersecting another body
+    bool isIntersecting(const Body& otherBody) const {
+        double distance = std::sqrt(std::pow(otherBody.getX() - x, 2) + std::pow(otherBody.getY() - y, 2));
+        return distance <= (radius + otherBody.getRadius());
+    }
+
+    //Absorb another body into this body
+    void absorb(Body& otherBody) {
+        //implement absorption with another body
+        //updates mass, radius, and velocity and acceleration following
+        //inelastic collision
 
 
+    }
 
-#endif
+    //Calculate elastic collision with another body
+    void calculateElasticCollision(Body& otherBody) {
+    //implement elastic collision
+
+    }
+};
+
+#endif // BODY_HPP
